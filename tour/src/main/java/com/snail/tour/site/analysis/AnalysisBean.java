1c5b601c8112ac97589c3fb80cd3dacdb99e77e7
David Meng <david.meng@xerox.com>
2015-02-13 16:00:17 +0800

    add bean

---
+++ tour/src/main/java/com/snail/tour/site/analysis/AnalysisBean.java
@@ -0,0 +1,80 @@
+package com.snail.tour.site.analysis;
+
+public class AnalysisBean {
+	
+	String urlPatternStr ;
+	
+	String keyStartStr ;
+	String keyEndStr ;
+	String keyCss ;
+	
+	String priceCss ; 
+	String priceStartStr ; 
+	String priceEndStr ;
+
+	String contentCss ;
+	String contentStartStr ;
+	String contentEndStr ;
+	
+	
+	public String getUrlPatternStr() {
+		return urlPatternStr;
+	}
+	public void setUrlPatternStr(String urlPatternStr) {
+		this.urlPatternStr = urlPatternStr;
+	}
+	public String getKeyStartStr() {
+		return keyStartStr;
+	}
+	public void setKeyStartStr(String keyStartStr) {
+		this.keyStartStr = keyStartStr;
+	}
+	public String getKeyEndStr() {
+		return keyEndStr;
+	}
+	public void setKeyEndStr(String keyEndStr) {
+		this.keyEndStr = keyEndStr;
+	}
+	public String getKeyCss() {
+		return keyCss;
+	}
+	public void setKeyCss(String keyCss) {
+		this.keyCss = keyCss;
+	}
+	public String getPriceCss() {
+		return priceCss;
+	}
+	public void setPriceCss(String priceCss) {
+		this.priceCss = priceCss;
+	}
+	public String getPriceStartStr() {
+		return priceStartStr;
+	}
+	public void setPriceStartStr(String priceStartStr) {
+		this.priceStartStr = priceStartStr;
+	}
+	public String getPriceEndStr() {
+		return priceEndStr;
+	}
+	public void setPriceEndStr(String priceEndStr) {
+		this.priceEndStr = priceEndStr;
+	}
+	public String getContentCss() {
+		return contentCss;
+	}
+	public void setContentCss(String contentCss) {
+		this.contentCss = contentCss;
+	}
+	public String getContentStartStr() {
+		return contentStartStr;
+	}
+	public void setContentStartStr(String contentStartStr) {
+		this.contentStartStr = contentStartStr;
+	}
+	public String getContentEndStr() {
+		return contentEndStr;
+	}
+	public void setContentEndStr(String contentEndStr) {
+		this.contentEndStr = contentEndStr;
+	}
+}
